723b20e312bc49b6daf26a576341d433b8786054
Camelia IGNAT <camelia.ignat@ext.ec.europa.eu>
2024-10-15 00:31:18 +0200

    Modify username, add domain, update user

--- invenio_saml/handlers.py
+++ invenio_saml/handlers.py
@@ -99,19 +99,19 @@ def update_user_data(user, account_info):
         user_data_changed = True
     
     # Update full name if it's different
-    if user.profile.full_name != account_info["user"]["profile"]["full_name"]:
-        user.profile.full_name = account_info["user"]["profile"]["full_name"]
+    if user.user_profile.full_name != account_info["user"]["profile"]["full_name"]:
+        user.user_profile.full_name = account_info["user"]["profile"]["full_name"]
         user_data_changed = True
         
     # Update affiliations if it's different
-    if user.profile.affiliations != account_info["user"]["profile"]["affiliations"]:
-        user.profile.affiliations = account_info["user"]["profile"]["affiliations"]
+    if user.user_profile.affiliations != account_info["user"]["profile"]["affiliations"]:
+        user.user_profile.affiliations = account_info["user"]["profile"]["affiliations"]
         user_data_changed = True
 
     # If any changes were made, commit the changes to the database
     #if user_data_changed:
     #    db.session.commit()
-    # current_app.logger.info("User data updated for user %s", user.email)
+    #    current_app.logger.info("User data updated for user %s", user.email)
 
     return user_data_changed
     
